4485a71a38ba9a4a1cb33edacfe7580ef2313a74
Mark Dai <mark19891107@users.noreply.github.com>
2018-04-10 14:46:57 +0800

    Create discourse-tag-webhook.js

---
+++ discourse-tag-webhook.js
@@ -0,0 +1,25 @@
+var request = require('request');
+
+module.exports = function (context,cb) {
+
+  var formData = {
+      raw: "我是 範本 哈哈",
+      title: "我是  範本   哈哈",
+      category: 12
+  };
+
+  request.post(
+    {
+      url: 'https://talk.pdis.nat.gov.tw/posts?api_key='+context.secrets.apikey+'&api_user='+context.secrets.apiuser,
+      formData: formData
+    },
+    function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        cb(null, err); 
+        return console.error('upload failed:', err);
+      }
+      cb(null, body); 
+      console.log('Upload successful!  Server responded with:', body);
+    }
+  );
+};
