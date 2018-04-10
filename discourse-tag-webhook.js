74b0ba78a96460f5fd7b1caa98765568554797d1
Mark Dai <mark19891107@users.noreply.github.com>
2018-04-10 17:42:45 +0800

    Updating discourse-tag-webhook.js

--- discourse-tag-webhook.js
+++ discourse-tag-webhook.js
@@ -1,25 +1,33 @@
 var request = require('request');
+// require('request').debug = true;
 
-module.exports = function (context,cb) {
+module.exports = function (context, cb) {
 
-  console.log(context.data)
-  var topic_id = context.data.topic.tags;
-  var formData = {
-    tags:['開放政府']
-  }
+  var topic_id = context.data.topic.id;
+  var topic_slug = context.data.topic.slug;
+  var topic_tags = context.data.topic.tags.slice();
+  var new_tags = context.data.topic.tags.slice();
+  
+  new_tags.push("開放政府")
 
-  request.put(
-    {
-      url: 'https://talk.pdis.nat.gov.tw/t/'+topic_id+'?api_key='+context.secrets.apikey+'&api_user='+context.secrets.apiuser,
-      formData: formData
-    },
-    function optionalCallback(err, httpResponse, body) {
-      if (err) {
-        cb(null, err); 
-        return console.error('upload failed:', err);
+  if (new_tags.some(val => topic_tags.indexOf(val) === -1)) {
+    request.put(
+      {
+        url: 'https://talk.pdis.nat.gov.tw/t/' + topic_slug + '/' + topic_id + '?api_key=' + context.secrets.apikey + '&api_user=' + context.secrets.apiuser,
+        formData: {"tags[]": new_tags}
+      },
+      function optionalCallback(err, httpResponse, body) {
+        if (err) {
+          cb(null, err);
+          return console.error('upload failed:', err);
+        }
+        cb(null, body);
+        console.log('Upload successful!  Server responded with:', body);
       }
-      cb(null, body); 
-      console.log('Upload successful!  Server responded with:', body);
-    }
-  );
+    );
+  }
 };
+
+function AddTags(old_tags){
+  return old_tags.push("開放政府");
+}
