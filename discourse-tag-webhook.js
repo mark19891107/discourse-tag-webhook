25297e20d295da4a1eedb21987e6787aa4456b1d
Mark Dai <mark19891107@users.noreply.github.com>
2018-04-10 15:01:32 +0800

    Updating discourse-tag-webhook.js

--- discourse-tag-webhook.js
+++ discourse-tag-webhook.js
@@ -7,18 +7,18 @@ module.exports = function (context,cb) {
   var formData = context.data;
   formData.category = 12;
 
-  request.post(
-    {
-      url: 'https://talk.pdis.nat.gov.tw/posts?api_key='+context.secrets.apikey+'&api_user='+context.secrets.apiuser,
-      formData: formData
-    },
-    function optionalCallback(err, httpResponse, body) {
-      if (err) {
-        cb(null, err); 
-        return console.error('upload failed:', err);
-      }
-      cb(null, body); 
-      console.log('Upload successful!  Server responded with:', body);
-    }
-  );
+  // request.post(
+  //   {
+  //     url: 'https://talk.pdis.nat.gov.tw/posts?api_key='+context.secrets.apikey+'&api_user='+context.secrets.apiuser,
+  //     formData: formData
+  //   },
+  //   function optionalCallback(err, httpResponse, body) {
+  //     if (err) {
+  //       cb(null, err); 
+  //       return console.error('upload failed:', err);
+  //     }
+  //     cb(null, body); 
+  //     console.log('Upload successful!  Server responded with:', body);
+  //   }
+  // );
 };
