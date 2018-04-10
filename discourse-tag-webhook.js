e6eaf981fdb041c1d3b2ab04bc2373a842001dcc
Mark Dai <mark19891107@users.noreply.github.com>
2018-04-10 17:30:58 +0800

    Updating discourse-tag-webhook.js

--- discourse-tag-webhook.js
+++ discourse-tag-webhook.js
@@ -2,23 +2,24 @@ var request = require('request');
 
 module.exports = function (context,cb) {
 
-  console.log(context.data.topic.title)
+  console.log(context.data)
+  var topic_id = context.data.topic.tags;
+  var formData = {
+    tags:['開放政府']
+  }
 
-  var formData = context.data.topic.title;
-  formData.category = 12;
-
-  // request.post(
-  //   {
-  //     url: 'https://talk.pdis.nat.gov.tw/posts?api_key='+context.secrets.apikey+'&api_user='+context.secrets.apiuser,
-  //     formData: formData
-  //   },
-  //   function optionalCallback(err, httpResponse, body) {
-  //     if (err) {
-  //       cb(null, err); 
-  //       return console.error('upload failed:', err);
-  //     }
-  //     cb(null, body); 
-  //     console.log('Upload successful!  Server responded with:', body);
-  //   }
-  // );
+  request.put(
+    {
+      url: 'https://talk.pdis.nat.gov.tw/t/'+topic_id+'?api_key='+context.secrets.apikey+'&api_user='+context.secrets.apiuser,
+      formData: formData
+    },
+    function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        cb(null, err); 
+        return console.error('upload failed:', err);
+      }
+      cb(null, body); 
+      console.log('Upload successful!  Server responded with:', body);
+    }
+  );
 };
